6df53b10b94ae442a84575371302919001c947af
ogrergo <louis.vanbeurden@gmail.com>
2023-02-08 21:27:47 -0500

    add reverse proxy

--- server/app.py
+++ server/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
-app = Flask(__name__)
-
-@app.route("/")
+bp = Blueprint('ricochet-robot', __name__,)
+@bp.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+app = Flask(__name__)
+app.register_blueprint(bp, url_prefix='/api')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
+
