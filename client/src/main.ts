dad10d02909c7f100f65e49f7c6f0550628c4c88
ogrergo <louis.vanbeurden@gmail.com>
2023-02-09 23:50:54 -0500

    add basic asyncio

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { io } from "socket.io-client";
 
 import './assets/main.css'
 
 const app = createApp(App)
+var socket = io();
+socket.on('connect', function () {
+    socket.emit('my event', { data: 'I\'m connected!' });
+});
 
 app.use(router)
 
--- server/app.py
+++ server/app.py
@@ -1,13 +1,22 @@
 from flask import Flask, Blueprint
+from flask_socketio import SocketIO
 
-bp = Blueprint('ricochet-robot', __name__,)
-@bp.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# HTTP API
+# bp = Blueprint('ricochet-robot', __name__,)
+# @bp.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
 
 app = Flask(__name__)
-app.register_blueprint(bp, url_prefix='/api')
+app.config['SECRET_KEY'] = 'secret!'
+# app.register_blueprint(bp, url_prefix='/api')
+
+# socketio0
+socketio = SocketIO(app) #, logger=True, engineio_logger=True)
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
-
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
